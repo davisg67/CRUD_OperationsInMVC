5568674d3ebc8133e31c76c9598349cf0de0769d
Greg Davis <gdavis@davtechsolutions.com>
2019-04-16 03:01:37 -0400

    Initial commit.

---
+++ CRUD_OperationsinMVC/Controllers/DepartmentController.cs
@@ -0,0 +1,20 @@
+﻿using CRUD_OperationsinMVC.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CRUD_OperationsinMVC.Controllers
+{
+    public class DepartmentController : Controller
+    {
+        // GET: Department
+        public ActionResult Index()
+        {
+            EmployeeDBContext dbContext = new EmployeeDBContext();
+            List<Department> listDepartments = dbContext.Departments.ToList();
+            return View(listDepartments);
+        }
+    }
+}
---
+++ CRUD_OperationsinMVC/Controllers/EmployeeController.cs
@@ -0,0 +1,27 @@
+﻿using CRUD_OperationsinMVC.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CRUD_OperationsinMVC.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        // GET: Employee
+        public ActionResult Index(int departmentId)
+        {
+            EmployeeDBContext dbContext = new EmployeeDBContext();
+            List<Employee> employees = dbContext.Employees.Where(emp => emp.DepartmentId == departmentId).ToList();
+            return View(employees);
+        }
+
+        public ActionResult Details(int id)
+        {
+            EmployeeDBContext dbContext = new EmployeeDBContext();
+            Employee employee = dbContext.Employees.Single(x => x.EmployeeId == id);
+            return View(employee);
+        }
+    }
+}
